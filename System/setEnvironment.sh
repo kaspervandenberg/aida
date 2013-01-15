30cfd383506ff40777b3f9555b31c6f1fb6b7cb0
Kasper van den Berg <kasper@kaspervandenberg.net>
2013-01-15 18:30:21 +0100

    - Add values from SOURCE_THIS.sh

--- System/setEnvironment.sh
+++ System/setEnvironment.sh
@@ -9,3 +9,12 @@ export CATALINA_HOME=/usr/local/tomcat
 export CATALINA_BASE=/usr/local/tomcat
 export INDEXDIR=/var/local/aida/indexes
 export JAVA_HOME=/usr/lib/jvm/jdk1.7.0
+export JRE_HOME=$JAVA_HOME
+export AIDA_HOME=/home/aida/
+export ANT_HOME=/usr/share/ant
+export TARGET_URL=http://vocab.maastro.nl:80
+export TARGET_HOST=vocab.maastro.nl
+export TARGET_PORT=80
+
+export PATH=$JAVA_HOME/bin:$CATALINA_HOME/bin:$ANT_HOME/bin:$PATH
+export EDITOR=nano

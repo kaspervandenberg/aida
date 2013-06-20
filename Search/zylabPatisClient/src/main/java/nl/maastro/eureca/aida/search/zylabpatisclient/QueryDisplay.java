d425e483cb7ce14237b193730121969d76b8c175
Kasper van den Berg <kasper.vandenberg@maastro.nl>
2013-06-20 17:28:10 +0200

    AIDA-31 - Display span queries.

--- Search/zylabPatisClient/src/main/java/nl/maastro/eureca/aida/search/zylabpatisclient/QueryDisplay.java
+++ Search/zylabPatisClient/src/main/java/nl/maastro/eureca/aida/search/zylabpatisclient/QueryDisplay.java
@@ -11,6 +11,11 @@ import org.apache.lucene.search.FuzzyQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TermRangeQuery;
+import org.apache.lucene.search.spans.SpanMultiTermQueryWrapper;
+import org.apache.lucene.search.spans.SpanNearQuery;
+import org.apache.lucene.search.spans.SpanOrQuery;
+import org.apache.lucene.search.spans.SpanQuery;
+import org.apache.lucene.search.spans.SpanTermQuery;
 
 /**
  *
@@ -39,6 +44,14 @@ public class QueryDisplay {
 			return dumpFuzzyQuery(indent, (FuzzyQuery)q);
 		} else if (q instanceof TermRangeQuery) {
 			return dumpTermRangeQuery(indent, (TermRangeQuery)q);
+		} else if (q instanceof SpanNearQuery) {
+			return dumpSpanNearQuery(indent, (SpanNearQuery)q);
+		} else if (q instanceof SpanOrQuery) {
+			return dumpSpanOrQuery(indent, (SpanOrQuery)q);
+		} else if (q instanceof SpanTermQuery) {
+			return dumpSpanTermQuery(indent, (SpanTermQuery)q);
+		} if(q instanceof SpanMultiTermQueryWrapper) {
+			return dumpSpanMultiTermWrapper(indent, (SpanMultiTermQueryWrapper<?>)q);
 		} else {
 			throw new Error(String.format("Unsupported Query type %s",q.getClass().getName()));
 		}
@@ -46,6 +59,11 @@ public class QueryDisplay {
 	
 	private String dumpTermQuery(String indent, TermQuery q) {
 		return String.format("%sTermQuery (%s): (%s, %s)\n",
+				indent, q.getClass().getName(), q.getTerm().field(), q.getTerm().text());
+	}
+
+	private String dumpSpanTermQuery(String indent, SpanTermQuery q) {
+		return String.format("%sSpanTermQuery (%s): (%s, %s)\n",
 				indent, q.getClass().getName(), q.getTerm().field(), q.getTerm().text());
 	}
 
@@ -78,5 +96,36 @@ public class QueryDisplay {
 				indent, q.getClass().getName(), q.getField(), 
 				q.getLowerTerm().utf8ToString(), q.getUpperTerm().utf8ToString());
 	}
-	
+
+	private String dumpSpanNearQuery(String indent, SpanNearQuery q) {
+		String subIndent = indent +"\t";
+		StringBuilder result = new StringBuilder(String.format("%sSpanNearQuery (slop:%s, type:%s) [\n",
+				indent,
+				Integer.valueOf(q.getSlop()).toString(),
+				q.getClass().getName()));
+		for (SpanQuery clause : q.getClauses()) {
+			result.append(dumpQuery(subIndent, clause));
+		}
+		result.append(indent);
+		result.append("]\n");
+		return result.toString();
+	}
+
+	private String dumpSpanOrQuery(String indent, SpanOrQuery q) {
+		String subIndent = indent +"\t";
+		StringBuilder result = new StringBuilder(String.format("%sSpanOrQuery (%s) [\n",
+				indent,
+				q.getClass().getName()));
+		for (SpanQuery clause : q.getClauses()) {
+			result.append(dumpQuery(subIndent, clause));
+		}
+		result.append(indent);
+		result.append("]\n");
+		return result.toString();
+	}
+
+	private String dumpSpanMultiTermWrapper(String indent, SpanMultiTermQueryWrapper<?> q) {
+		return String.format("%sSpan MT wrapper (%s): %s", 
+			indent, q.getClass().getName(), q.toString(""));
+	}
 }

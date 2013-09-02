e7c77a2e0233dbde9742b9b9eeac43d589e10c7a
Kasper van den Berg <kasper.vandenberg@maastro.nl>
2013-09-02 14:20:31 +0200

    AIDA-10 -CancelableTask.

---
+++ Search/Indexer/src/main/java/nl/maastro/eureca/aida/indexer/CancelableTask.java
@@ -0,0 +1,83 @@
+// © Maastro Clinic, 2013
+package nl.maastro.eureca.aida.indexer;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Base class for threads support cancelation; for example an infinite loop in a thread that runs until {@link #cancel() cancled}.
+ * Derived classes should expect {@link InterruptedException} and should stop when {@link #isCancelled()} returns true.
+ *
+ * @author Kasper van den Berg <kasper.vandenberg@maastro.nl> <kasper@kaspervandenberg.net>
+ */
+public abstract class CancelableTask extends Thread {
+	/**
+	 * Flag set when {@link #cancel()} was called.
+	 * 
+	 * @see #isCancelled()
+	 * @see #cancel()
+	 */
+	private final AtomicBoolean canceled = new AtomicBoolean(false);
+
+	public CancelableTask() {
+	}
+
+	public CancelableTask(Runnable target) {
+		super(target);
+	}
+
+	public CancelableTask(ThreadGroup group, Runnable target) {
+		super(group, target);
+	}
+
+	public CancelableTask(String name) {
+		super(name);
+	}
+
+	public CancelableTask(ThreadGroup group, String name) {
+		super(group, name);
+	}
+
+	public CancelableTask(Runnable target, String name) {
+		super(target, name);
+	}
+
+	public CancelableTask(ThreadGroup group, Runnable target, String name) {
+		super(group, target, name);
+	}
+
+	public CancelableTask(ThreadGroup group, Runnable target, String name, long stackSize) {
+		super(group, target, name, stackSize);
+	}
+	
+	
+	
+	/**
+	 * Indicate this thread should stop.  The thread receives an {@link InterruptedException} and {@link #isCancelled()} will 
+	 * return true; the derived class is responsible for actually stopping.
+	 */
+	public void cancel() {
+		canceled.set(true);
+		this.interrupt();
+	}
+
+	/**
+	 * Derived threads should regularly check {@code isCancelled} and stop when {@code isCancelled} returns {@code true}.
+	 * 
+	 * @return <ul><li>{@code true}, when the thread was canceled;</li>
+	 * 			<li>{@code false}, when the thread is allowed to continue.</li></ul>
+	 */
+	public boolean isCancelled() {
+		return canceled.get();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * <p>{@code start()} allows restarting a Thread by setting {@link #isCancelled()} to false.</p>
+	 */
+	@Override
+	public synchronized void start() {
+		canceled.set(false);
+		super.start();
+	}
+}
---
+++ Search/Indexer/src/test/java/nl/maastro/eureca/indexer/CancelableTaskTest.java
@@ -0,0 +1,136 @@
+// © Maastro Clinic, 2013
+package nl.maastro.eureca.indexer;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import nl.maastro.eureca.aida.indexer.CancelableTask;
+import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.junit.Assume.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ *
+ * @author Kasper van den Berg <kasper.vandenberg@maastro.nl> <kasper@kaspervandenberg.net>
+ */
+public class CancelableTaskTest {
+	private final long TESTED_SLEEP = 20;
+	private final long TESTER_SLEEP = 50;
+	
+	private CancelableTask tested;
+	private AtomicInteger changeableValue;
+	private AtomicBoolean interruptedFlag;
+	
+	public CancelableTaskTest() {
+	}
+	
+	@BeforeClass
+	public static void setUpClass() {
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+	}
+	
+	@Before
+	public void setUp() {
+		changeableValue = new AtomicInteger(0);
+		interruptedFlag = new AtomicBoolean(false);
+		
+		tested = createTask();
+	}
+	
+	@After
+	public void tearDown() {
+		tested.cancel();
+		try {
+			tested.join();
+		} catch (InterruptedException ex) {
+			
+		}
+		tested = null;
+		
+	}
+	
+	@Test
+	public void testIsCanceled() {
+		tested.cancel();
+		assertThat("canceled", tested.isCancelled(), is(true));
+	}
+
+	@Test
+	public void testThreadRuns() {
+		try {
+			assumeThat(changeableValue.get(), is(0));
+			tested.start();
+				Thread.sleep(TESTER_SLEEP);
+				assertThat("thread changed value", changeableValue.getAndSet(0), not(0));
+		} catch(InterruptedException ex) {
+			assumeNoException(ex);
+		}
+	}
+
+	@Test
+	public void testThreadStops() {
+		try {
+			assumeThat(changeableValue.get(), is(0));
+			tested.start();
+			Thread.sleep(TESTER_SLEEP);
+			
+			tested.cancel();
+			Thread.sleep(TESTER_SLEEP);
+			changeableValue.set(0);
+			for (int i = 0; i < 10; i++) {
+				Thread.sleep(500);
+				assertThat("thread not changeing value", changeableValue.getAndSet(0), is(0));
+			}
+		} catch(InterruptedException ex) {
+			assumeNoException(ex);
+		}
+	}
+
+	@Test
+	public void testThreadInterrupted() {
+		final int N_ATTEMPTS = 10;
+		int nInterrupted = 0;
+		try {
+			for (int i = 0; i < 10; i++) {
+				interruptedFlag.set(false);
+				CancelableTask tested = createTask();
+				tested.start();
+				Thread.sleep(TESTER_SLEEP);
+				tested.cancel();
+				tested.join();
+				if(interruptedFlag.get()) {
+					nInterrupted++;
+				}
+					
+			}
+			assertThat(String.format("At least one of %s interrupted", N_ATTEMPTS),
+					nInterrupted, greaterThanOrEqualTo(1));
+		} catch (InterruptedException ex) {
+			assumeNoException(ex);
+		}	
+	}
+
+	private CancelableTask createTask() {
+		return new CancelableTask() {
+			@Override
+			public void run() {
+				while (!isCancelled()) {
+					try {
+							changeableValue.set(1);
+							sleep(TESTED_SLEEP);
+					} catch (InterruptedException ex) {
+						interruptedFlag.set(true);
+						interrupted();
+					}
+				}
+			} };	
+	}
+}

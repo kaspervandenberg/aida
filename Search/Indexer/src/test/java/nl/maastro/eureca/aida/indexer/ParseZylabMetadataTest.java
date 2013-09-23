dff1a18a8d762c0f0a9a8799290fbadabd7dc94c
Kasper van den Berg <kasper.vandenberg@maastro.nl>
2013-09-23 12:20:38 +0200

    AIDA-10 reuse mocked resolver.

--- Search/Indexer/src/test/java/nl/maastro/eureca/aida/indexer/ParseZylabMetadataTest.java
+++ Search/Indexer/src/test/java/nl/maastro/eureca/aida/indexer/ParseZylabMetadataTest.java
@@ -1,38 +1,27 @@
 // © Maastro Clinic, 2013
 package nl.maastro.eureca.aida.indexer;
 
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import nl.maastro.eureca.aida.indexer.tika.parser.ZylabMetadataXml;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.junit.Assume.*;
 import org.junit.Before;
-import org.mockito.Mock;
-import static org.mockito.Mockito.*;
 import static org.hamcrest.Matchers.*;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import static nl.maastro.eureca.aida.indexer.matchers.LunceneMatchers.*;
+import nl.maastro.eureca.aida.indexer.mocked.Resolver;
 
 /**
  *
  * @author Kasper van den Berg <kasper.vandenberg@maastro.nl> <kasper@kaspervandenberg.net>
  */
 public class ParseZylabMetadataTest {
-	private static final String METADATA_RESOURCE = "/datadir/{C2212583-3E6D-4AB2-8F80-2C8934833CAB}.xml";
-	private static final String REF_PATH = "D:\\ZyIMAGE Data\\Index Data\\EMD\\txt\\2012\\52\\00000000\\";
-	private static final String REF_FILE = "50003BX4.TXT"; 
 	private static final String ID = "{C2212583-3E6D-4AB2-8F80-2C8934833CAB}"; 
 	private static final String EXP_PATIS_NR = "12345";
 			
 	
 	private ZylabData data;
 	private ParseZylabMetadata testee;
-	private URL metadataUrl;
-	private URL dataUrl;
-	@Mock private ReferenceResolver referenceResolver;
+	private Resolver mockedResolver;
+	private ReferenceResolver referenceResolver;
 	
 	public ParseZylabMetadataTest() {
 	}
@@ -42,17 +31,10 @@ public class ParseZylabMetadataTest {
 		MockitoAnnotations.initMocks(this);
 		data = new ZylabData();
 
+		mockedResolver = new Resolver();
+		referenceResolver = mockedResolver.getResolver();
 
-		try {
-			metadataUrl = ParseZylabMetadataTest.class.getResource(METADATA_RESOURCE);
-			dataUrl = new URL("http://unexisting.localhost/data/data.doc");
-//			when (referenceResolver.resolve(new ZylabMetadataXml.FileRef(REF_PATH, REF_FILE))).thenReturn(dataUrl);
-			when (referenceResolver.resolve(Mockito.any(ZylabMetadataXml.FileRef.class))).thenReturn(dataUrl);
-		} catch (MalformedURLException | URISyntaxException ex) {
-			assumeNoException(ex);
-		}
-
-		testee = new ParseZylabMetadata(data, metadataUrl, referenceResolver);
+		testee = new ParseZylabMetadata(data, mockedResolver.getMetadataURL(), referenceResolver);
 	}
 
 	@Test
@@ -87,6 +69,6 @@ public class ParseZylabMetadataTest {
 	public void testHasDataUrl() throws Exception {
 		testee.call();
 
-		assertThat("Data URL as expected", data.getDataUrl(), is(dataUrl));
+		assertThat("Data URL as expected", data.getDataUrl(), is(mockedResolver.getDataUrl()));
 	}
 }
---
+++ Search/Indexer/src/test/java/nl/maastro/eureca/aida/indexer/mocked/Resolver.java
@@ -0,0 +1,59 @@
+// © Maastro Clinic, 2013
+package nl.maastro.eureca.aida.indexer.mocked;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import nl.maastro.eureca.aida.indexer.ReferenceResolver;
+import nl.maastro.eureca.aida.indexer.tika.parser.ZylabMetadataXml;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import static org.junit.Assume.*;
+
+/**
+ *
+ * @author Kasper van den Berg <kasper.vandenberg@maastro.nl> <kasper@kaspervandenberg.net>
+ */
+public class Resolver {
+	private static final String METADATA_RESOURCE = "/datadir/{C2212583-3E6D-4AB2-8F80-2C8934833CAB}.xml";
+	private static final String REF_PATH = "D:\\ZyIMAGE Data\\Index Data\\EMD\\txt\\2012\\52\\00000000\\";
+	private static final String REF_FILE = "50003BX4.TXT"; 
+	
+	private boolean isInitialised = false;
+	@Mock private ReferenceResolver referenceResolver;
+	private URL metadataUrl;
+	private URL dataUrl;
+	
+	public Resolver() {
+	}
+
+	private void init() {
+		try {
+			metadataUrl = Resolver.class.getResource(METADATA_RESOURCE);
+			dataUrl = new URL("http://unexisting.localhost/data/data.doc");
+			
+			MockitoAnnotations.initMocks(this);
+			
+			when (referenceResolver.resolve(Mockito.any(ZylabMetadataXml.FileRef.class))).thenReturn(dataUrl);
+		} catch (MalformedURLException | URISyntaxException ex) {
+			assumeNoException(ex);
+		}
+	}
+
+	public ReferenceResolver getResolver() {
+		if(!isInitialised) {
+			init();
+		}
+		return referenceResolver;
+	}
+
+	public URL getMetadataURL() {
+		return metadataUrl;
+	}
+
+	public URL getDataUrl() {
+		return dataUrl;
+	}
+}

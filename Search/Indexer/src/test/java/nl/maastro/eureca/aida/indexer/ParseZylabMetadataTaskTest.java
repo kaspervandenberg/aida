02056b5a0230bc0760a4c4190dfdebdf48f28061
Kasper van den Berg <kasper.vandenberg@maastro.nl>
2013-09-26 14:09:38 +0200

    AIDA-10 - ParseZylabMetadataTest tests theories about required fields.

--- Search/Indexer/src/test/java/nl/maastro/eureca/aida/indexer/ParseZylabMetadataTaskTest.java
+++ Search/Indexer/src/test/java/nl/maastro/eureca/aida/indexer/ParseZylabMetadataTaskTest.java
@@ -1,22 +1,41 @@
 // © Maastro Clinic, 2013
 package nl.maastro.eureca.aida.indexer;
 
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.Map;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.junit.Assume.*;
 import org.junit.Before;
 import static org.hamcrest.Matchers.*;
 import org.mockito.MockitoAnnotations;
 import static nl.maastro.eureca.aida.indexer.matchers.LunceneMatchers.*;
 import nl.maastro.eureca.aida.indexer.mocked.Resolver;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
 /**
  *
  * @author Kasper van den Berg <kasper.vandenberg@maastro.nl> <kasper@kaspervandenberg.net>
  */
+@RunWith(Theories.class)
 public class ParseZylabMetadataTaskTest {
-	private static final String ID = "{C2212583-3E6D-4AB2-8F80-2C8934833CAB}"; 
-	private static final String EXP_PATIS_NR = "12345";
-			
+	private static final String EXPECTED_ID = "{C2212583-3E6D-4AB2-8F80-2C8934833CAB}"; 
+	private static final String EXPECTED_PATIS_NR = "12345";
+	@SuppressWarnings("serial")
+	private static Map<FieldsToIndex, String> EXPECTED_VALUES = Collections.unmodifiableMap(
+			new EnumMap<FieldsToIndex, String>(FieldsToIndex.class) {{
+		put(FieldsToIndex.ID, ParseZylabMetadataTaskTest.EXPECTED_ID);
+		put(FieldsToIndex.PATISNUMMER, ParseZylabMetadataTaskTest.EXPECTED_PATIS_NR);
+	}});
+
+	@DataPoints
+	public static FieldsToIndex[] FIELDS = {
+		FieldsToIndex.ID, FieldsToIndex.PATISNUMMER
+	};
 	
 	private ZylabData data;
 	private ParseZylabMetadataTask testee;
@@ -42,27 +61,22 @@ public class ParseZylabMetadataTaskTest {
 		testee.call();
 	}
 
-	@Test
-	public void testIdParsed() throws Exception {
+	@Theory
+	public void testHasField(FieldsToIndex field) throws Exception {
 		testee.call();
 
-		assertThat("has field id", data.getFields(), hasItem(fieldNamed(FieldsToIndex.ID.fieldName)));
+		assertThat(String.format("has field %s", field.name()), data.getFields(), hasItem(fieldNamed(field.fieldName)));
 	}
 
-	@Test
-	public void testHasPatisNumberParsed() throws Exception {
+	@Theory
+	public void testHasFieldWithCorrectValue(FieldsToIndex field) throws Exception {
+		assumeTrue(EXPECTED_VALUES.containsKey(field));
+		
 		testee.call();
 
-		assertThat("has field PatisNr", data.getFields(), hasItem(fieldNamed(FieldsToIndex.PATISNUMMER.fieldName)));
-	}
-
-	@Test
-	public void testPatisNumberParsed() throws Exception {
-		testee.call();
-
-		assertThat("has correct PatisNr", data.getFields(), hasItem(allOf(
-				fieldNamed(FieldsToIndex.PATISNUMMER.fieldName),
-				fieldValue(ParseZylabMetadataTaskTest.EXP_PATIS_NR))));
+		assertThat("has correct field–value", data.getFields(), hasItem(allOf(
+				fieldNamed(field.fieldName),
+				fieldValue(EXPECTED_VALUES.get(field)))));
 	}
 
 	@Test

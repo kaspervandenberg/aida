949f8001764b02abd22dd0bb93b38e9381a7f302
Kasper van den Berg <kasper.vandenberg@maastro.nl>
2013-11-06 11:50:02 +0100

    AIDA-83 Test to pinpoint problems with parsing RDF:Turtle.
    Classloading problemens where caused by version conflicts between different versions of commons-io, dependnencies in previous commit fix this.

---
+++ Search/zylabPatisClient/src/test/java/nl/maastro/eureca/aida/search/zylabpatisclient/validation/TurtleParserTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.maastro.eureca.aida.search.zylabpatisclient.validation;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.openrdf.rio.turtle.TurtleParser;
+
+/**
+ *
+ * @author kasper
+ */
+public class TurtleParserTest {
+	
+	public TurtleParserTest() {
+	}
+
+	@Test
+	public void testCreateTurtleParser() {
+		TurtleParser p = new TurtleParser();
+	}
+}

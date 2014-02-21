a21db3f631a2936094486a60c5eb0af47f3cdef2
Kasper van den Berg <kasper@kaspervandenberg.net>
2014-02-21 12:08:08 +0100

    [FRESH START] Concept for Lucene query.

---
+++ cleanslate/src/main/java/nl/maastro/eureca/lucene_rdf/concepts/lucene_query/Query_Expression.java
@@ -0,0 +1,11 @@
+package nl.maastro.eureca.lucene_rdf.concepts.lucene_query;
+
+/**
+ * Top level interface representing queries in Lucene.
+ */
+public interface Query_Expression {
+
+}
+
+/* vim:set tabstop=4 shiftwidth=4 : */
+

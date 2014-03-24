335fe5bd813ed4f92de63960a11cdc0c1b1b93e9
Kasper van den Berg <kasper@kaspervandenberg.net>
2014-03-24 13:45:30 +0100

    [cleanslate] javadoc for concepts.lucenequery.binding package.

---
+++ cleanslate/src/main/java/nl/maastro/eureca/lucenerdf/concepts/lucenequery/binding/package-info.java
@@ -0,0 +1,16 @@
+// © Kasper van den Berg, 2014
+/**
+ * Concepts for parts of Lucene queries that can vary.
+ * The concepts in this package extend the collection of building blocks for
+ * Lucene queries defined in {@link 
+ * nl.maastro.eureca.lucenerdf.concepts.lucenequery} with variables that
+ * refer to 
+ * {@link nl.maastro.eureca.lucenerdf.concepts.lucenequery.QueryExpression}s.
+ * 
+ * <p>The hierarchy of classes in this package mirrors the one in {@link 
+ * nl.maastro.eureca.lucenerdf.concepts.lucenequery}.</p>
+ */
+package nl.maastro.eureca.lucenerdf.concepts.lucenequery.binding;
+
+/* vim:set tabstop=4 shiftwidth=4 autoindent spell spelllang=en_gb : */
+

1436f6ffe813a7d62018edac96306d7d774f714e
Kasper van den Berg <kasper@kaspervandenberg.net>
2014-03-24 14:29:58 +0100

    [cleanslate] Add top level Query interface.

---
+++ cleanslate/src/main/java/nl/maastro/eureca/lucenerdf/concepts/lucenequery/Query.java
@@ -0,0 +1,17 @@
+// © Kasper van den Berg, 2014
+package nl.maastro.eureca.lucenerdf.concepts.lucenequery;
+
+/**
+ * A {@link QueryExpression} that can be submitted to Lucene.
+ *
+ * <p>{@code Query} complements {@link TokenExpression}: whereas
+ * {@code TokenExpressions} must be contained in other {@code QueryExpressions},
+ * while {@code Queries} model concepts that can be the root of a
+ * {@code QueryExpression} tree.</p>
+ */
+interface Query extends QueryExpression {
+}
+
+/* vim:set tabstop=4 shiftwidth=4 autoindent spell spelllang=en_gb : */
+
+
